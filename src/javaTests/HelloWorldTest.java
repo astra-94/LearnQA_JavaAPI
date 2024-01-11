555a8b67eb1b8ddea5393c8ed57ae550ca6d5fe8
Nataliia Makeeva <n.makeeva@paysend.com>
2024-01-11 19:10:42 +0300

    Just test

---
+++ src/javaTests/HelloWorldTest.java
@@ -0,0 +1,13 @@
+package javaTests;
+
+import org.junit.jupiter.api.Test;
+
+public class HelloWorldTest {
+
+    @Test
+    public void testHelloWorld()
+    {
+        System.out.println("Hello, world!");
+    }
+
+}
